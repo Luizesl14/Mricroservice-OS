ea4dd30d9c36835641494f57131059352f81a145
Luizesl <luiz@bluetechno.com.br>
2022-03-06 21:23:16 -0300

    Removendo dependencias

--- system-order-producer/src/main/java/com/systemorderproducer/insfrastructure/http/ObjectNotfoundException.java
+++
@@ -1,7 +0,0 @@
-package com.systemorderproducer.insfrastructure.http;
-
-public class ObjectNotfoundException extends Exception{
-    public ObjectNotfoundException(String message) {
-        super(String.format("uid: " + message + " Não encontrado"));
-    }
-}
--- system-order-producer/src/main/java/com/systemorderproducer/insfrastructure/repositories/IOrderProducerRepository.java
+++
@@ -1,9 +0,0 @@
-package com.systemorderproducer.insfrastructure.repositories;
-
-import com.systemorderproducer.domain.model.OrderProducer;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface IOrderProducerRepository extends JpaRepository<OrderProducer,Integer> {
-}
